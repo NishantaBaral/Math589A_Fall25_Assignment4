d4fc6287b528abc6d3ce78845e69670b499ab29f
Nishanta Baral <nishantabaral48@gmail.com>
2025-12-02 13:08:13 -0700

    1st try

--- fp_wrapper.py
+++ fp_wrapper.py
@@ -5,22 +5,28 @@ _libpath = os.path.join(_here, "libfixed_point.so")
 _lib = ctypes.CDLL(_libpath)
 
 _lib.fixed_point_solve.argtypes = [
-    ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_int,
-    ctypes.POINTER(ctypes.c_double),
-    ctypes.POINTER(ctypes.c_double),
-    ctypes.POINTER(ctypes.c_int)
+    ctypes.c_double,                 # x1_0
+    ctypes.c_double,                 # x2_0
+    ctypes.c_double,                 # tol
+    ctypes.c_int,                    # max_iter
+    ctypes.POINTER(ctypes.c_double), # x1_out
+    ctypes.POINTER(ctypes.c_double), # x2_out
+    ctypes.POINTER(ctypes.c_int),    # iters_out
+    ctypes.c_double                  # scale  <-- NEW
 ]
+
 _lib.fixed_point_solve.restype = ctypes.c_int
 
-def solve_system(x1_0, x2_0, tol, max_iter):
+def solve_system(x1_0, x2_0, tol, max_iter,scale=0.1):
     x1_out = ctypes.c_double()
     x2_out = ctypes.c_double()
     iters_out = ctypes.c_int()
 
     rc = _lib.fixed_point_solve(
         x1_0, x2_0, tol, max_iter,
-        ctypes.byref(x1_out), ctypes.byref(x2_out), ctypes.byref(iters_out)
+        ctypes.byref(x1_out), ctypes.byref(x2_out), ctypes.byref(iters_out),scale
     )
     if rc != 0:
         raise RuntimeError(f"C solver error {rc}")
     return x1_out.value, x2_out.value, iters_out.value
+
